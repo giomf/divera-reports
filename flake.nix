7ee87f7aaaf6efe41733e4fa044b3a78a9cccf19
Guillaume Fournier <me@guif.dev>
2024-10-16 09:03:16 +0200

    Add nixosmodule

--- flake.nix
+++ flake.nix
@@ -1,20 +1,29 @@
 {
-  description = "A very basic rust flake";
+  description = "Divera reports flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    {
+      nixosModules.default = import ./module.nix self;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk' = pkgs.callPackage naersk { };
+      in rec{
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ pkg-config openssl ];
+          buildInputs = with pkgs; [ openssl ];
+        };
+        overlays.default = final: prev: { divera-reports = defaultPackage; };
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt openssl pkg-config];
-          env = {
-          };
+          buildInputs = with pkgs; [ cargo rustc rust-analyzer rustfmt openssl pkg-config ];
+          env = { };
         };
       });
 }
---
+++ module.nix
@@ -0,0 +1,67 @@
+flake:
+{ config, lib, pkgs, ... }:
+with lib;
+
+let
+  service_name = "divera-reports";
+  cfg = config.services.${service_name};
+  pkg = (flake.defaultPackage.${pkgs.stdenv.hostPlatform.system});
+
+in {
+  options.services.${service_name} = {
+    enable = mkEnableOption "enable ${service_name} services";
+    config_path = mkOption {
+      type = types.path;
+      description = "The config path";
+    };
+    timer = mkOption {
+      type = types.str;
+      description = "The timer value to set";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services."${service_name}-station" = {
+      description = "Uploads station divera reports";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkg}/bin/divera-reports --config ${cfg.config_path} report station --upload Verbesserungsvorschläge_Feuerwehrhaus.xlsx";
+        ProtectHome = "read-only";
+      };
+    };
+
+    systemd.services."${service_name}-roster" = {
+      description = "Uploads roster divera reports";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkg}/bin/divera-reports --config ${cfg.config_path} report roster --upload Vorschläge_Dienstplan.xlsx";
+        ProtectHome = "read-only";
+      };
+    };
+
+    systemd.services."${service_name}-absences" = {
+      description = "Uploads absences divera reports";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkg}/bin/divera-reports --config ${cfg.config_path} report absences --upload Abwesenheiten.xlsx";
+        ProtectHome = "read-only";
+      };
+    };
+
+    # Define the systemd target to group multiple services
+    systemd.targets.${service_name} = {
+      description = "Group of divera reports services";
+      bindsTo = [ "${service_name}-station.service" "${service_name}-roster.service" "${service_name}-absences.service"];
+    };
+
+    systemd.timers.${service_name} = {
+      description = "${service_name} timer";
+      wantedBy = [ "timers.target" ]; # Ensure the timer is activated at boot
+      timerConfig = {
+        OnCalendar = cfg.timer; 
+        Persistent = true; # Ensures the job runs after missed events (e.g., after reboot)
+        Unit = "${service_name}.target";
+      };
+    };
+  };
+}
